402bba45fc00a9a726e5a397b12323f7331602c4
Barbara <barbara.martin.brown@gmail.com>
2023-11-20 14:01:30 +0100

    cambios

--- curso_udemy/seccion24/IntroduccionPHP_inicio/01-hola-mundo.php
+++ curso_udemy/seccion24/IntroduccionPHP_inicio/01-hola-mundo.php
@@ -1,9 +1,14 @@
 <?php
 // VIDEO 255
-echo "hola mundo con echo";
-print "hola mundo con rpinterest";
-
+echo "hola mundo (con echo)";
+echo "<br/>";
+print "hola mundo (con print)";
+echo "<br/>";
+echo "var_dump dice el tipo de dato: ";
 var_dump(20);
 
+
+
 ?>
-<h1>Hola mundo con HTML</h1>
+<h1>Hola mundo (solo en HTML)</h1>
+<h1>Hola mundo<?php echo " (php en HTML)"; ?></h1>
